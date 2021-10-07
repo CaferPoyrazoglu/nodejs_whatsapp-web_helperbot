55b5b7b5e74e7db7786242f034860f792a29778d
CaferPoyrazoglu <cafererhan@gmail.com>
2021-10-08 01:20:09 +0300

    Goltas icin duzenlendi.

--- index.js
+++ index.js
@@ -26,93 +26,74 @@ client.on("qr", (qr) => {
 });
 
 client.on("ready", () => {
-  console.log("Client is ready!");
+  console.log("Sunucu Hazir!");
 });
 
 client.on("auth_failure", (msg) => {
-  // Fired if session restore was unsuccessfull
-  console.error("AUTHENTICATION FAILURE", msg);
+  console.error("Dogrulama Hatasi!", msg);
 });
 
 client.on("ready", () => {
-  console.log("READY");
+  console.log("Calisiyor!");
 });
 
 client.on("message", async (msg) => {
-  console.log("MESSAGE RECEIVED", msg);
+  let chat = await msg.getChat();
 
-  if (msg.body === "Dosya") {
-    let chat = await msg.getChat();
-    const media = MessageMedia.fromFilePath("D://a.png");
+  if (
+    chat.isGroup &&
+    msg.body === "Rapor" &&
+    msg.id.remote === "905494032745-1633642858@g.us"
+  ) {
+    const media = MessageMedia.fromFilePath("D://rapor.xlsx");
     chat.sendMessage(media);
   }
 });
 
 client.on("message_create", (msg) => {
-  // Fired on all message creations, including your own
   if (msg.fromMe) {
-    // do stuff here
   }
 });
 
 client.on("message_revoke_everyone", async (after, before) => {
-  // Fired whenever a message is deleted by anyone (including you)
-  console.log(after); // message after it was deleted.
+  console.log(after);
   if (before) {
-    console.log(before); // message before it was deleted.
+    console.log(before);
   }
 });
 
 client.on("message_revoke_me", async (msg) => {
-  // Fired whenever a message is only deleted in your own view.
-  console.log(msg.body); // message before it was deleted.
+  console.log(msg.body);
 });
 
 client.on("message_ack", (msg, ack) => {
-  /*
-      == ACK VALUES ==
-      ACK_ERROR: -1
-      ACK_PENDING: 0
-      ACK_SERVER: 1
-      ACK_DEVICE: 2
-      ACK_READ: 3
-      ACK_PLAYED: 4
-  */
-
   if (ack == 3) {
-    // The message was read
   }
 });
 
 client.on("group_join", (notification) => {
-  // User has joined or been added to the group.
   console.log("join", notification);
-  notification.reply("User joined.");
 });
 
 client.on("group_leave", (notification) => {
-  // User has left or been kicked from the group.
   console.log("leave", notification);
-  notification.reply("User left.");
 });
 
 client.on("group_update", (notification) => {
-  // Group picture, subject or description has been updated.
   console.log("update", notification);
 });
 
 client.on("change_battery", (batteryInfo) => {
-  // Battery percentage for attached device has changed
   const { battery, plugged } = batteryInfo;
-  console.log(`Battery: ${battery}% - Charging? ${plugged}`);
+  console.log(`Pil Durumu: ${battery}% - Sarj oluyor! ${plugged}`);
 });
 
 client.on("change_state", (state) => {
-  console.log("CHANGE STATE", state);
+  console.log("Durum degisti: ", state);
 });
 
 client.on("disconnected", (reason) => {
-  console.log("Client was logged out", reason);
+  console.log("Cikis yapildi!", reason);
 });
 
 client.initialize();
